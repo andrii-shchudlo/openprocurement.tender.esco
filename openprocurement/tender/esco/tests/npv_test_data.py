b3545d8fc71b07bd29e6e466315d942847d01ff8
Tuoris <tuoris.dev@gmail.com>
2017-08-21 11:46:54 +0300

    Add test data for payments percentage changing

--- openprocurement/tender/esco/tests/npv_test_data.py
+++ openprocurement/tender/esco/tests/npv_test_data.py
@@ -134,3 +134,67 @@ ANNOUNCEMENT_DATE_CHANGING = {
         },
     ]
 }
+
+PAYMENTS_PERCENTAGE_CHANGING = {
+    'yearlyPaymentsPercentage': [
+        0.7100,
+        0.7200,
+        0.7300,
+        0.7400,
+        0.7500,
+        0.7600,
+        0.7700,
+        0.7800,
+        0.7900,
+        0.8000,
+    ],
+    'annualCostsReduction': ANNUAL_COSTS_REDUCTION,
+    'contractDuration': {
+        'years': 2,
+        'days': 10,
+    },
+    'announcementDate': '2017-08-18',
+    'NBUdiscountRate': NBU_DISCOUNT_RATE,
+    'calculated': [
+        {
+            'amountContract': 359.866028767123,
+            'amountPerformance': 1509.25419393209
+        },
+        {
+            'amountContract': 364.934564383562,
+            'amountPerformance': 1505.00489590214
+        },
+        {
+            'amountContract': 370.003100000000,
+            'amountPerformance': 1500.75559787220
+        },
+        {
+            'amountContract': 375.071635616438,
+            'amountPerformance': 1496.50629984225
+        },
+        {
+            'amountContract': 380.140171232877,
+            'amountPerformance': 1492.25700181231
+        },
+        {
+            'amountContract': 385.208706849315,
+            'amountPerformance': 1488.00770378236
+        },
+        {
+            'amountContract': 390.277242465753,
+            'amountPerformance': 1483.75840575242
+        },
+        {
+            'amountContract': 395.345778082192,
+            'amountPerformance': 1479.50910772247
+        },
+        {
+            'amountContract': 400.414313698630,
+            'amountPerformance': 1475.25980969253
+        },
+        {
+            'amountContract': 405.482849315069,
+            'amountPerformance': 1471.01051166258
+        },
+    ]
+}
