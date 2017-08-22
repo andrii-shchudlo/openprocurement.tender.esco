7b014fd2cb8758d8b5b3db39506d4d4bcfcf5f3f
Tuoris <tuoris.dev@gmail.com>
2017-08-22 15:46:29 +0300

    Fix some PEP8 issues

--- openprocurement/tender/esco/npv_calculation.py
+++ openprocurement/tender/esco/npv_calculation.py
@@ -33,11 +33,13 @@ def calculate_discount_rate(
 
     return Fraction(str(nbu_discount_rate)) * Fraction(days_for_discount_rate, days_per_year)
 
+
 def calculate_discount_rates(
         days_for_discount_rates,
         nbu_discount_rate,
         days_per_year=DAYS_PER_YEAR):
     '''Calculates discount rates from days_for_discount_rates list'''
+
     return [
         calculate_discount_rate(
             days_for_discount_rate,
